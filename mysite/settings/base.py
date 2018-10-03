d6aa8af9d39fdda6f0e8c253dae8a705b568bfdd
Ignacio Salazar <isalazar@teravisiontech.com>
2018-10-03 12:59:41 -0500

    fix allowed hosts

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$ldwp&agzxkb$brgp2^0gw*nxtrwr)lv&u^y^66j=57bq3xwoe'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
